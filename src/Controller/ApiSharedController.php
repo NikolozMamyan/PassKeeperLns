f5026aad618d2dedfa8c52785e568ba4ca570e7c
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2025-05-16 10:57:05 +0200

    shared api

---
+++ src/Controller/ApiSharedController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiSharedController extends AbstractController
+{
+#[Route('/api/shared', name: 'api_shared_credentials', methods: ['GET', 'OPTIONS'])]
+public function apiShared(
+    Request $request,
+    SharedAccessRepository $sharedAccessRepository,
+    EncryptionService $encryptionService,
+    UserRepository $userRepository
+): Response {
+    // CORS preflight
+    if ($request->getMethod() === 'OPTIONS') {
+        return new Response('', 204);
+    }
+
+    // Auth via token (X-AUTH-TOKEN header)
+    $token = $request->headers->get('X-AUTH-TOKEN');
+    if (!$token) {
+        return $this->json(['error' => 'Token manquant'], Response::HTTP_UNAUTHORIZED);
+    }
+
+    $user = $userRepository->findOneBy(['apiToken' => $token]);
+    if (!$user) {
+        return $this->json(['error' => 'Token invalide'], Response::HTTP_UNAUTHORIZED);
+    }
+
+    $sharedAccesses = $sharedAccessRepository->findSharedWith($user);
+
+    $data = array_map(function (SharedAccess $access) use ($encryptionService) {
+        $credential = $access->getCredential();
+        return [
+            'id' => $credential->getId(),
+            'domain' => $credential->getDomain(),
+            'username' => $credential->getUsername(),
+            'password' => $encryptionService->decrypt($credential->getPassword()),
+            'name' => $credential->getName(),
+            'shared_by' => $access->getOwner()->getEmail(),
+        ];
+    }, $sharedAccesses);
+
+    return $this->json(['credentials' => $data]);
+}
+
+}
